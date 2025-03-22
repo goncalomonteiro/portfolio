d6bf712be02528d85921ac3f430d5567b0321a2f
evaine <enoxy1998@gmail.com>
2025-03-22 05:44:07 +0000

    Change sidebar labels

--- components/IconVertical.tsx
+++ components/IconVertical.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Puzzle from '@/assets/img/icons/puzzle.svg'
 import Profile from '@/assets/img/icons/experience-expert.svg'
-import Portfolio from '@/assets/img/icons/home8.svg'
+import Portfolio from '@/assets/img/icons/portfolio.svg'
 import Image from 'next/image'
 import ThemeToggle from './ThemeToggle'
 
@@ -16,7 +16,7 @@ const IconVertical = () => {
         <Image src={Profile} alt="" /> <span>Expertise</span>
         </a>
         <a className="list-group-item list-group-item-action" href="#list-item-2">
-          <Image src={Puzzle} alt="" /> <span>Skill Set</span>
+          <Image src={Puzzle} alt="" /> <span>Tech Stack</span>
         </a>
         <a className="list-group-item list-group-item-action" href="#list-item-3">
         <Image src={Portfolio} alt="" /> <span>Portfolio</span>
