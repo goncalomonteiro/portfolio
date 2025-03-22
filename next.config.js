e98a9438522075893af41a5bb3e4dcea9d1dcf3d
evaine <enoxy1998@gmail.com>
2025-03-22 11:17:19 +0000

    add local deploy configs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
-  trailingSlash: true, // helps ensure consistent URL structure
-  images: {
-    unoptimized: true, // disable image optimization for static export
-  },
+
 };
 
 module.exports = nextConfig;
