d3a3af22cf33f2bf0b1fa2d1bd7892f001207e1c
evaine <enoxy1998@gmail.com>
2025-03-23 23:19:03 +0000

    add configs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    basePath: '/portfolio',
+    assetPrefix: '/portfolio/',
+    trailingSlash: true, // helps ensure consistent URL structure
+    images: {
+      unoptimized: true, // disable image optimization for static export
+    },
 };
 
 module.exports = nextConfig;
